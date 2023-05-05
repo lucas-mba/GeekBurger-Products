eb6fd968b28a412568df46c25c513895637e6aa3
luccunha <lucas.cunha@capgemini.com>
2023-05-04 21:32:56 -0300

    Adding tests

--- GeekBurger.Products.UnitTests/Controllers/ProductsControllerUnitTests.cs
+++ GeekBurger.Products.UnitTests/Controllers/ProductsControllerUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using FluentAssertions;
+using GeekBurger.Products.Contract;
 using GeekBurger.Products.Controllers;
 using GeekBurger.Products.Model;
 using GeekBurger.Products.Repository;
@@ -19,11 +20,13 @@ namespace GeekBurger.Products.UnitTests.Controllers
         private readonly ProductsController _productsController;
         private Mock<IProductsRepository> _productRepositoryMock;
         private Mock<IMapper> _mapperMock;
+        private MockRepository _mockRepository;
 
         public ProductsControllerUnitTests()
         {
-            _productRepositoryMock = new Mock<IProductsRepository>();
-            _mapperMock = new Mock<IMapper>();
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _productRepositoryMock = _mockRepository.Create<IProductsRepository>();
+            _mapperMock = _mockRepository.Create<IMapper>();
             _productsController = new ProductsController(_productRepositoryMock.Object, _mapperMock.Object);
         }
 
@@ -42,6 +45,60 @@ namespace GeekBurger.Products.UnitTests.Controllers
             //assert            
             Assert.IsType<NotFoundObjectResult>(response);
             response.Should().BeEquivalentTo(expected);
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void OnGetProductsByStoreName_WhenProductIsFound_ShouldReturnProduct()
+        {
+            //arrange
+            var storeName = "Paulista";            
+            var product = new Product
+            {
+                ProductId = Guid.NewGuid(),
+            };
+
+            var productList = new List<Product> { product };
+            _productRepositoryMock.Setup(_ => _.GetProductsByStoreName(storeName)).Returns(productList);            
+
+            //act
+            var response = _productsController.GetProductsByStoreName(storeName);
+
+            //assert            
+            Assert.IsType<OkObjectResult>(response);
+        }
+
+        [Fact]
+        public void OnGetProductsByStoreName_WhenProductIsFound_ShouldReturnProductToGet()
+        {
+            //arrange
+            var storeName = "Paulista";
+            var guid = Guid.NewGuid();
+
+            var product = new Product
+            {
+                ProductId = guid
+            };
+
+            var productToGet = new ProductToGet
+            {
+                ProductId = guid
+            };
+
+            var productList = new List<Product> { product };
+            var productToGetList = new List<ProductToGet> { productToGet };
+
+            _productRepositoryMock.Setup(_ => _.GetProductsByStoreName(storeName)).Returns(productList);
+            _mapperMock.Setup(_ => _.Map<IEnumerable<ProductToGet>>(productList)).Returns(productToGetList);
+            var expected = new OkObjectResult(productToGetList);
+
+            //act
+            var response = _productsController.GetProductsByStoreName(storeName);
+
+            //assert            
+            Assert.IsType<OkObjectResult>(response);
+            response.Should().BeEquivalentTo(expected);
+            _mockRepository.VerifyAll();
         }
     }
 }
